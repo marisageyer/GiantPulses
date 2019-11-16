e21eae9cd314a9784c5660518d8c1ffb1b2ca562
Marisa Geyer <marisa.geyer@gmail.com>
2019-11-16 13:14:52 +0000

    minor changes to giant_pulse and make_totals

--- giant_pulse_search_B0540.py
+++ giant_pulse_search_B0540.py
@@ -20,9 +20,9 @@ label_size = 8
 mpl.rcParams['xtick.labelsize'] = label_size 
 
 parser = argparse.ArgumentParser(usage='describe usage here',
-                                     description='description of script here.',epilog='Copyright (C) 2019 by Jones Chilufya')
-parser.add_argument('-dir', dest='directory_name', metavar='<directory_name>', help='specify directory where archive directories with pulsar observations are')
-parser.add_argument('-mask', dest='mask_file', metavar='<mask_file>', help='specify mask file where RFI mask lists are')
+                                     description='description of script here.',epilog='Copyright (C) 2019 by Marisa Geyer and Jones Chilufya')
+parser.add_argument('-dir', dest='directory_name', metavar='<directory_name>', help='specify directory where single pulse archives directories are')
+parser.add_argument('-mask', dest='mask_file', metavar='<mask_file>', help='specify mask file where RFI mask')
 parser.add_argument('-ext', dest='file_ext', metavar='<file_ext>', help='specify the file extensions in dir in which to search for giant pulses. Default = "fullband"')
 parser.add_argument('-nch', dest='num_chan', metavar='<num_chan>', type=int, help='specify the number of channels to fscrunch to before searching for giants. Default = 256')
 parser.add_argument('-v', dest='verbose', action="store_true", help='verbose output')
@@ -150,6 +150,9 @@ for snr_file in snr_data_files:
         idx_max = np.argmax(plot_profile[0,0,0,:])
         phase_max = phase_values[idx_max]	
 
+        
+        ph_lim_Aug = 0.0
+        ph_lim_Aug2 = 0.5 
         #ph_lim0 = 0.30
         #ph_lim1 = 0.45
         #ph_lim2 = 0.50
@@ -158,51 +161,53 @@ for snr_file in snr_data_files:
 
 
         #if (phase_max > ph_lim0 and phase_max < ph_lim1) or (phase_max > ph_lim2 and phase_max < ph_lim3):
-        fig, (ax1, ax2) = plt.subplots(2,1, sharex=True)
-        fig.subplots_adjust(hspace=0)
-        ax1.plot(phase_values, plot_profile[0,0,0,:], label='SNR = %.2f'%(snr))
 
-        #ax1.fill_betweenx(y=[np.min(plot_profile[0,0,0,:]),np.max(plot_profile[0,0,0,:])], x1=ph_lim0,x2=ph_lim1, alpha=0.4, color='b')
-        #ax1.fill_betweenx(y=[np.min(plot_profile[0,0,0,:]),np.max(plot_profile[0,0,0,:])], x1=ph_lim2,x2=ph_lim3, alpha=0.4, color='b')
-        #ax1.axvline(x=ph_lim0, color='r', alpha=0.2, lw=2.0)
-        #ax1.axvline(x=ph_lim1, color='r', alpha=0.2, lw=2.0)
-        #ax1.axvline(x=ph_lim2, ls='dashed',alpha=0.2)
-        #ax1.axvline(x=ph_lim3, ls='dashed',alpha=0.2)
-        ax1.axvline(x=phase_max, ls='dotted',alpha=0.3)
-        
-        ax1.set_title((os.path.basename(args.directory_name),snr_file),fontsize=10) ###
-        ax1.set_ylabel('Intensity', fontsize=10)
-        ax1.legend(loc='best')
-        spec_data = freq_data.reshape(nchan,nbin)
-        
-        data_weighted = np.ones((nchan, nbin))
-        for n in range(nchan):
-            data_weighted[n] =  weights[n]*spec_data[n,:]
-        
-        lower_freq = archive_file.get_centre_frequency() - archive_file.get_bandwidth()/2.0
-        high_freq = archive_file.get_centre_frequency() + archive_file.get_bandwidth()/2.0
-        min_phase = 0
-        max_phase = 1
-        if args.directory_name.endswith("/"):
-            saven = os.path.basename(args.directory_name[0:-1])
-        else:
-            saven = os.path.basename(args.directory_name)
-        figname = '%s_%s_SNR_%.2f_phase_%.2f.png' %(saven,snr_file, snr, phase_max)
-        ax2.imshow(data_weighted, extent=(min_phase,max_phase,lower_freq, high_freq), aspect='auto', cmap='magma', origin='lower')
-        ax2.set_xlabel('Pulse Phase', fontsize=10)
-        ax2.set_ylabel('Frequency (MHz)',fontsize=10)
-        #ax2.tick_params(axis='both', which='minor', labelsize=6)
-        ax3 = ax2.twinx()
-        ax3.set_ylim(0,nchan)
-        ax3.set_ylabel('Channel Number')
-        plt.rcParams.update({'figure.max_open_warning': 0})
-        plt.savefig(os.path.join(figpath, figname))
+        if (phase_max > ph_lim_Aug and phase_max < ph_lim_Aug2):
+            fig, (ax1, ax2) = plt.subplots(2,1, sharex=True)
+            fig.subplots_adjust(hspace=0)
+            ax1.plot(phase_values, plot_profile[0,0,0,:], label='SNR = %.2f'%(snr))
 
-        print "----------------------------------------------------------------"
-        print "----------------------------------------------------------------"
-        print 'figure saved as',os.path.join(figpath, figname)
+            #ax1.fill_betweenx(y=[np.min(plot_profile[0,0,0,:]),np.max(plot_profile[0,0,0,:])], x1=ph_lim0,x2=ph_lim1, alpha=0.4, color='b')
+            #ax1.fill_betweenx(y=[np.min(plot_profile[0,0,0,:]),np.max(plot_profile[0,0,0,:])], x1=ph_lim2,x2=ph_lim3, alpha=0.4, color='b')
+            #ax1.axvline(x=ph_lim0, color='r', alpha=0.2, lw=2.0)
+            #ax1.axvline(x=ph_lim1, color='r', alpha=0.2, lw=2.0)
+            #ax1.axvline(x=ph_lim2, ls='dashed',alpha=0.2)
+            #ax1.axvline(x=ph_lim3, ls='dashed',alpha=0.2)
+            ax1.axvline(x=phase_max, ls='dotted',alpha=0.3)
+            
+            ax1.set_title((os.path.basename(args.directory_name),snr_file),fontsize=10) ###
+            ax1.set_ylabel('Intensity', fontsize=10)
+            ax1.legend(loc='best')
+            spec_data = freq_data.reshape(nchan,nbin)
+            
+            data_weighted = np.ones((nchan, nbin))
+            for n in range(nchan):
+                data_weighted[n] =  weights[n]*spec_data[n,:]
+            
+            lower_freq = archive_file.get_centre_frequency() - archive_file.get_bandwidth()/2.0
+            high_freq = archive_file.get_centre_frequency() + archive_file.get_bandwidth()/2.0
+            min_phase = 0
+            max_phase = 1
+            if args.directory_name.endswith("/"):
+                saven = os.path.basename(args.directory_name[0:-1])
+            else:
+                saven = os.path.basename(args.directory_name)
+            figname = '%s_%s_SNR_%.2f_phase_%.2f.png' %(saven,snr_file, snr, phase_max)
+            ax2.imshow(data_weighted, extent=(min_phase,max_phase,lower_freq, high_freq), aspect='auto', cmap='magma', origin='lower')
+            ax2.set_xlabel('Pulse Phase', fontsize=10)
+            ax2.set_ylabel('Frequency (MHz)',fontsize=10)
+            #ax2.tick_params(axis='both', which='minor', labelsize=6)
+            ax3 = ax2.twinx()
+            ax3.set_ylim(0,nchan)
+            ax3.set_ylabel('Channel Number')
+            plt.rcParams.update({'figure.max_open_warning': 0})
+            plt.savefig(os.path.join(figpath, figname))
 
-        print "----------------------------------------------------------------"
-        print "----------------------------------------------------------------"
-        #plt.show()
-        plt.close()
+            print "----------------------------------------------------------------"
+            print "----------------------------------------------------------------"
+            print 'figure saved as',os.path.join(figpath, figname)
+
+            print "----------------------------------------------------------------"
+            print "----------------------------------------------------------------"
+            #plt.show()
+            plt.close()
--- make_totals.py
+++ make_totals.py
@@ -13,10 +13,9 @@ def list_files(directory, extension):
 
 parser = argparse.ArgumentParser(usage='describe usage here',
                                      description='description of script here.',epilog='Copyright (C) 2019 by Marisa Geyer')
-parser.add_argument('-dir', dest='directory_name', metavar='<directory_name>', help='specify directory where archive directories with pulsar observations with "fullband" extenstion (as produced by add_freq_bands.py) are')
-#parser.add_argument('-par', dest='par_file', metavar='<par_file>', help='specify par file')
-#parser.add_argument('-ext', dest='save_ext', metavar='<save_ext>', help='specify extension to save')
-#parser.add_argument('-save_dir', dest='save_dir', metavar='<save_dir>', help='specify directory to save to')
+parser.add_argument('-dir', dest='directory_name', metavar='<directory_name>', help='specify directory where archive directories with single pulse archives with extension args.ext are')
+parser.add_argument('-ext', dest='ext', metavar='<ext>', help='specify extension of files to be added. Default is fullband.')
+parser.add_argument('-save_ext', dest='save_ext', metavar='<save_ext>', help='specify directory to save to. Default is total')
 args = parser.parse_args()
 
 #dirname = '/scratch02/mgeyer/Jones_HonsProj/SEARCH_DATA/Marchdata/Singles_MBpar'
@@ -27,16 +26,37 @@ dirlisttouse = dirlist
 os.chdir(dirname)
 
 
+if not args.ext:
+   ext = 'fullband'
+else: 
+   ext = args.ext
+
+if not args.save_ext:
+   save_ext = 'total'
+else:
+   save_ext = args.save_ext
+
+
+searchstr = '*'+ext 
+
+
 for dir in dirlisttouse:
     print dir
-    totalfilename = '%s_totalfull' %dir
+    totalfilename = '%s.%s' %(dir,save_ext)
     print totalfilename
     dirpath = os.path.join(dirname,dir)
     print dirpath
     os.chdir(dirpath)
     
-    command = ['psradd', '-v', '-o', totalfilename, '*fullband']
-    zap_file = subprocess.Popen(command, shell=False, cwd='.', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-    (stdoutdata, stderrdata) = zap_file.communicate()
-    return_code = zap_file.returncode
+    command = ['psradd', '-v', '-o', totalfilename, searchstr]
+    added_file = subprocess.Popen(command, shell=False, cwd='.', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    (stdoutdata, stderrdata) = added_file.communicate()
+    return_code = added_file.returncode
     #print stdoutdata.split("\r")
+
+
+    commandfold = ['pam', '-e', 'T', '-T', totalfilename]
+    fold_file = subprocess.Popen(commandfold, shell=False, cwd='.', stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    (stdout, stderr) = fold_file.communicate()
+    print stdout
+
